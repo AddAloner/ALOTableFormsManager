f518ec7d37367494d9dedca181f4f7a671e9be15
Alexey Yachmenov <aloner.brn@gmail.com>
2014-04-10 23:34:59 +0400

    Disable selection style for custom cells

---
+++ ALOTableFormsManager/Cells/ALODateFieldTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ALODateFieldTableViewCell.h
+//  ALOTableFormsManagerExample
+//
+//  Created by Alexey Yachmenov on 10.04.14.
+//  Copyright (c) 2014 Alexey Yachmenov. All rights reserved.
+//
+
+#import "ALOTextFieldTableViewCell.h"
+
+@interface ALODateFieldTableViewCell : ALOTextFieldTableViewCell
+
+@end
---
+++ ALOTableFormsManager/Items/ALOTableFormsDateField.m
@@ -0,0 +1,13 @@
+//
+//  ALOTableFormsDateField.m
+//  ALOTableFormsManagerExample
+//
+//  Created by Alexey Yachmenov on 10.04.14.
+//  Copyright (c) 2014 Alexey Yachmenov. All rights reserved.
+//
+
+#import "ALOTableFormsDateField.h"
+
+@implementation ALOTableFormsDateField
+
+@end
