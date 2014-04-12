42419dc97dbf758e27d2641cfb15dfc4817d2444
Alexey Yachmenov <aloner.brn@gmail.com>
2014-04-12 11:52:15 +0400

    Complete number input

---
+++ ALOTableFormsManager/ALOAccessoryView.h
@@ -0,0 +1,13 @@
+//
+//  ALOAccessoryView.h
+//  ALOTableFormsManagerExample
+//
+//  Created by Alexey Yachmenov on 12.04.14.
+//  Copyright (c) 2014 Alexey Yachmenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALOAccessoryView : UIToolbar
+
+@end
---
+++ ALOTableFormsManager/ALOAccessoryView.m
@@ -0,0 +1,31 @@
+//
+//  ALOAccessoryView.m
+//  ALOTableFormsManagerExample
+//
+//  Created by Alexey Yachmenov on 12.04.14.
+//  Copyright (c) 2014 Alexey Yachmenov. All rights reserved.
+//
+
+#import "ALOAccessoryView.h"
+
+@implementation ALOAccessoryView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
